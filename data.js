b710ac02ac873f490694d3ee82ee71edc0387627
NickSarode <93203828+NickSarode@users.noreply.github.com>
2021-12-17 22:10:19 +0000

    update data.js
    Javascript functions to show currencies from api is written and data.js updated

--- data.js
+++ data.js
@@ -23,3 +23,61 @@ data1 = [{
   'abbr' : 'JPY',
   'val' : 21.6455
 }]
+
+const url = "http://api.exchangeratesapi.io/v1/latest?access_key=f27c66a897df264865447f0c2c682894"
+fetch(url).then((response)=>{
+  return response.json();  // converting byte data to json
+}).then(data=>{
+  //console.log(data)
+  //console.log(data.rates)
+  var ratesStr = JSON.stringify(data.rates);
+  let ratesObj = Object.entries(data.rates);
+  //console.log(ratesObj[1][0])
+  //console.log(ratesObj);
+  document.getElementById("strRatesObj").value = ratesObj
+  let cur = '';
+//   Object.entries(data.rates).forEach((entry) => {
+//   const [key, value] = entry;
+//   //console.log(`${key}: ${value}`);
+//   let key1 = entry[0];
+//   let value1 = entry[1];
+//   var mainContainer = document.getElementById("currTable");
+//   cur += ' <tr> '
+//          ' <td> '+ key +' </td>'
+//          ' <td> '+ value + ' </td>'
+//          ' </tr> '
+//   //   var div = document.createElement("div");
+// });
+  cur += '<table class="table table-dark table-hover justify-content-center">'+
+        '<thead class="table table-dark">'+
+        '<tr>'+
+        '<th>   </th>'+
+        '<th>Currency</th>'+
+        '<th>Rate</th>'+
+        '</tr>'+
+        '</thead>'+
+        '<tbody>'
+  Object.entries(data.rates).forEach(entry => {
+    let key = entry[0];
+    let value = entry[1];
+    rates = ' '+ key + value + ' ';
+    //var mainContainer = document.getElementById("currTable");
+    //var div = document.createElement("div");
+    cur += ' <tr> ' +
+                    ' <td> '+ ' ' +' </td>' +
+                    ' <td> '+ key +' </td>' +
+                    ' <td> '+ value + ' </td>' +
+                    ' </tr> ';
+    //mainContainer.appendChild(div);
+    //console.log(key, value);
+  });
+  cur +=
+    '</tbody> </table>'
+
+   $('div.container').append(cur)
+   //console.log(cur)
+  //console.log(rates)
+}).catch(error=>{
+    // logging error
+   console.log(error);
+})
