c386be7bd608ee772eadea260f97f42d8a851455
NickSarode <93203828+NickSarode@users.noreply.github.com>
2021-12-17 12:42:32 +0000

    create data.js
    Created data.js and data1 json object is declared in js file.

---
+++ data.js
@@ -0,0 +1,25 @@
+data1 = [{
+  'name' : 'us dollar',
+  'abbr' : 'USD',
+  'val' : 1.1562
+},{
+  'name' : 'indian rupee',
+  'abbr' : 'INR',
+  'val' : 86.725
+},{
+  'name' : 'aus dollar',
+  'abbr' : 'AUD',
+  'val' : 1.64567
+},{
+  'name' : 'ca dollar',
+  'abbr' : 'CAD',
+  'val' : 1.7466
+},{
+  'name' : 'chinese yuan',
+  'abbr' : 'CNY',
+  'val' : 7.8545
+},{
+  'name' : 'japanese yen',
+  'abbr' : 'JPY',
+  'val' : 21.6455
+}]
