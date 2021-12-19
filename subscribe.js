cda90668438c5d7e3cab644ef6c8680e6ffbaf2c
NickSarode <93203828+NickSarode@users.noreply.github.com>
2021-12-19 19:05:41 +0000

    Create subscribe.js
    js file created for functions

---
+++ subscribe.js
@@ -0,0 +1,97 @@
+// function submitform(){
+//    alert("Sending Json");
+//      let email = document.getElementById('email').value;
+//      let name = document.getElementById('name').value;
+//      let currency = document.getElementById('currencyDropdown').value;
+//      let threshold = document.getElementById('threshold').value;
+//      let condition = document.getElementById('condition').value;
+//    alert(email + ' ' + name + ' ' + currency + ' ' + threshold + ' ' + condition);
+//    var todo = {
+//        "email": email,
+//        "name" : name,
+//        "currency": currency,
+//        "threshold": threshold,
+//        "condition": condition
+//    };
+//    alert(JSON.stringify(todo));
+// //    $.ajax({
+// //     type: 'POST',
+// //     url: 'http://localhost:8080/addUser',
+// //     data: JSON.stringify(todo),
+// //     contentType: 'application/json',
+// //     success: function(data) {
+// //       alert("success");
+// //     }
+// // });
+// // try{
+// //   const response = await fetch('http://localhost:8080/addUser',{
+// //   method: "POST",
+// //   body: JSON.stringify(todo),
+// //   headers: {
+// //     'Content-Type':'application/json'
+// //   }
+// // });
+// // } catch(e){
+// //   console.error(e);
+// //   alert('there is an error');
+// // }
+//
+//
+//  }
+
+function fetchUD(){
+  debugger;
+  var email = document.getElementById("email").value;
+  // alert(email)
+  const url = "http://localhost:8080/getUser/"+email;
+  // alert(url)
+  console.log(1)
+  console.log(url)
+
+  $.ajax({
+      type: "GET",
+      //url: "http://api.exchangeratesapi.io/v1/latest?access_key=f27c66a897df264865447f0c2c682894",
+      url: url,
+      data: "{}",
+      success: function (data) {
+          console.log(data);
+          //let details = ''
+          var dataList = Object.entries(data)
+          console.log(dataList);
+          var details = []
+          details = dataList[1]
+          var d = details[1]
+          //var obj = {d}
+          console.log(d)
+          //console.log('data: '+ d[0]);
+          console.log(d.length);
+          console.log(d[0])
+          var name = d[0]['name']
+                var email_add = d[0]['email']
+                var currency = d[0]['currency']
+                var threshold = d[0]['threshold']
+                var condition = d[0]['condition']
+          console.log(name + ' ' + email_add + ' ' + currency + ' ' + threshold + ' ' + condition)
+          $("#name").val(name);
+                 document.getElementById('name').value = name;
+                 document.getElementById('currencyDropdown').value = currency;
+                 document.getElementById('threshold').value = threshold;
+                 document.getElementById('condition').value = condition;
+          console.log("success");
+          // if(d.length > 0){
+          //   console.log(d[0])
+          //       var name = d[0]['name']
+          //       var email_add = d[0]['email']
+          //       var currency = d[0]['currency']
+          //       var threshold = d[0]['threshold']
+          //       var condition = d[0]['condition']
+          //       alert(name + ' ' + email_add + ' ' + currency + ' ' + threshold + ' ' + condition);
+          //        document.getElementById("name").value = name;
+          //        document.getElementById("currencyDropdown").value = currency;
+          //        document.getElementById("threshold").value = threshold;
+          //        document.getElementById("condition").value = condition;
+          // }
+      }
+  });
+
+}
