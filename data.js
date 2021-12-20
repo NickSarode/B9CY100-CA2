18eddc3a05421a493512131ee88c3fc6b1e40823
NickSarode <93203828+NickSarode@users.noreply.github.com>
2021-12-20 14:09:39 +0000

    Update data.js
    API and front end application has been deployed on vm hence the url to fetch the data is updated

--- data.js
+++ data.js
@@ -1,30 +1,30 @@
-data1 = [{
-  'name' : 'us dollar',
-  'abbr' : 'USD',
-  'val' : 1.1562
-},{
-  'name' : 'indian rupee',
-  'abbr' : 'INR',
-  'val' : 86.725
-},{
-  'name' : 'aus dollar',
-  'abbr' : 'AUD',
-  'val' : 1.64567
-},{
-  'name' : 'ca dollar',
-  'abbr' : 'CAD',
-  'val' : 1.7466
-},{
-  'name' : 'chinese yuan',
-  'abbr' : 'CNY',
-  'val' : 7.8545
-},{
-  'name' : 'japanese yen',
-  'abbr' : 'JPY',
-  'val' : 21.6455
-}]
+// data1 = [{
+//   'name' : 'us dollar',
+//   'abbr' : 'USD',
+//   'val' : 1.1562
+// },{
+//   'name' : 'indian rupee',
+//   'abbr' : 'INR',
+//   'val' : 86.725
+// },{
+//   'name' : 'aus dollar',
+//   'abbr' : 'AUD',
+//   'val' : 1.64567
+// },{
+//   'name' : 'ca dollar',
+//   'abbr' : 'CAD',
+//   'val' : 1.7466
+// },{
+//   'name' : 'chinese yuan',
+//   'abbr' : 'CNY',
+//   'val' : 7.8545
+// },{
+//   'name' : 'japanese yen',
+//   'abbr' : 'JPY',
+//   'val' : 21.6455
+// }]
 
-const url = "http://localhost:8080/getRates"
+const url = "http://104.40.132.4:5000/getRates"
 fetch(url).then((response)=>{
   return response.json();  // converting byte data to json
 }).then(data=>{
